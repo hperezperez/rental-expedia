8f7b93eea109506e8eb7694d1a547b542ceef211
Humberto Perez <ferdinero121312@gmail.com>
2015-07-19 00:19:50 -0700

    homework phase1

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 	<title>Data Design Proyect Phase 1 </title>
 	<body>
 		<header>
-			<h1> JOSE SANCHEZ</h1>
+			<h1> JOSE SANCHEZ Persona</h1>
 		</header>
 		<p> He is school teacher. He likes extreme sports. He  likes speed. He is single with one kid and one dog. He travels in the summer.</p>
 		<img src="images/actionfoto.jpg" alt="action"
-
-		<p>the first use case is A person rent a car by accesing the site, deciding the price and clicking.</p>
+		<h3>Use cases</h3>
+		<p>The first use case is A person rent a car by accesing the site, deciding the price and clicking.</p>
 		<p>The  second use case is A person rent a car  by accessing  the site,reserve the car later rent the car</p>
 		<p> The third use case is a person try to rent a car by accesing the site make reservation but never reserve it </p>
 		<h1>Entity Relationship Diagram</h1>
--- index2.php
+++ index2.php
@@ -1,19 +1,18 @@
 <! DOCTYPE html>
-<hmtl>
+<hmtl lang ="en">
 
 	<title>Data Design Proyect Phase 1 </title>
 	<body>
 		<header>
-			<h1> JOSE SANCHEZ</h1>
+			<h1> JOSE SANCHEZ Persona</h1>
 		</header>
-		<p> He is school teacher. He likes extreme sport. HE likes speed. He is single with one kid and one dog. He travels in the summer.</p>
-
-			<h3>Use cases</h3>
+		<p> He is school teacher. He likes extreme sports. He  likes speed. He is single with one kid and one dog. He travels in the summer.</p>
+		<img src="images/actionfoto.jpg" alt="action"
+		<h3>Use cases</h3>
 		<p>The first use case is A person rent a car by accesing the site, deciding the price and clicking.</p>
 		<p>The  second use case is A person rent a car  by accessing  the site,reserve the car later rent the car</p>
 		<p> The third use case is a person try to rent a car by accesing the site make reservation but never reserve it </p>
 		<h1>Entity Relationship Diagram</h1>
-		<img src="images/grafica1.jpg.bmp"alt="diagram"/>
+		<img src="images/grafica1.jpg.bmp"alt="diagram"
 	</body>
-
 </hmtl>
